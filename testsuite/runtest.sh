4298a2c6662a3167ccd403e40aa46a2341b70def
David Haworth <dh@thelancashireman.org>
2020-11-30 23:52:34 +0100

    Fix problem in runtest script; add expected results

--- testsuite/runtest.sh
+++ testsuite/runtest.sh
@@ -20,4 +20,17 @@
 # along with Certhas.  If not, see <http://www.gnu.org/licenses/>.
 
 (cd testsuite; gcc -g -o testprog testprog.c testdata.c)
-./explore.py testsuite/testprog uc1
+./explore.py testsuite/testprog \
+	uc1 us1 ui1 ul1 \
+	sc1 ss1 si1 sl1 \
+	p1 p2 p3 p4 p5 p6 p7 p8 p9 \
+	struct1a struct1b \
+	union1a union1b \
+	fp1 \
+	> results.txt
+diff -q results.txt testsuite/expected.txt
+if [ $? = 0 ]; then
+	echo "TEST PASSED"
+else
+	echo "TEST FAILED"
+fi
